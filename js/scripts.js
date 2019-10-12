6ba184bbcf81e26db613c8378cf13bda2d964977
Dekoworld <karituderrick@gmail.com>
2019-10-12 18:56:15 +0300

    add items for sale

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 //User Interface Logic
 $(document).ready(function (event) {
     /////Landing Page Btns
@@ -10,4 +9,14 @@ $(document).ready(function (event) {
     $("#delivery-btn").click(function () {
         $("#address").show();
         $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+    });
+    $("form#address-form").submit(function (event) {
+        event.preventDefault();
+        var streetAddress = $("input#street-add").val();
+        var city = $("input#city-add").val();
+        var zipcode = $("input#zip-add").val();
+        var newAddress = new Address(streetAddress, city, zipcode)
+        $("#order-content").show();
+        $("#landing-content").hide();
+        $("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
     });
